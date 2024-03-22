9b336a2ec2fe32b5c5fcb2756f033f5aab83093a
Tara Koomen <tarakoomen88@gmail.com>
2024-03-22 16:40:56 +1100

    Created a Yelp app

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :show, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # get 'restaurant/:id', to: 'restaurants#show', as: :restaurant
+  # post 'restaurants', to: 'restaurants#create'
+  # A visitor can add a new review to a restaurant
+  # GET "restaurants/38/reviews/new"
+  # POST "restaurants/38/reviews"
 
   # Defines the root path route ("/")
   # root "posts#index"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "44 0405 405 111", category: "french"}
+pizza_east = {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "44 0234 333 111", category: "italian"}
+kebab_west = {name: "Kebab West", address: "65B Westditch High St, London W1 9PQ", phone_number: "44 0234 333 111", category: "italian"}
+shrooooom = {name: "Shrooooom", address: "100 Zoooom St, London SE12 10B", phone_number: "44 98765 432 321", category: "belgian"}
+burgermeister = {name: "Burgermeister", address: "1 High St, London S1 6UK", phone_number: "44 04040 333 111", category: "japanese"}
+
+[dishoom, pizza_east, kebab_west, shrooooom, burgermeister].each do |attributes|
+restaurant = Restaurant.create!(attributes)
+puts "Created #{restaurant.name}"
+end
+puts "Finished!"
